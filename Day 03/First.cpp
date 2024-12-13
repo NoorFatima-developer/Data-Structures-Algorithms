e2e8f5b38b520211c9d0d7602404d12b24be99f4
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-14 00:29:00 +0500

    code is added

--- Day 03/First.cpp
+++ Day 03/First.cpp
@@ -1,6 +1,12 @@
 
 #include <iostream>
+using namespace std;
 int main()
 {
-    std::cout << "Welcome to" << " " ;
+    // Both output statements are true;
+    // Jb b hum cout and cin likhty hain tu osklye hmy std:: likhna prta hai islye meny
+    // oper using namespace std, kea hai ta k mjhy bar bar std:: na likhna pry...
+    // std::cout << "Hello, world!" << std::endl;"
+    cout << "Welcome to\n";
+    cout << "Welcome to Noor" << endl;
 };
