91741e7b95f45390a6dccac5a2e77a5fb5a9f357
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-17 18:50:52 +0500

    code is added

---
+++ Day 04/Second.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Enter your first integer: ";
+    cin >> num1;
+
+    cout << "Enter your second integer: ";
+    cin >> num2;
+
+    cout << "Sum of two integers is: "<< num1 + num2 << endl;
+}
