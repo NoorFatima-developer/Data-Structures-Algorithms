e5392bdaaf6b4ce4493dfe3ee7ebe48ab9fa7794
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-18 19:58:08 +0500

    code is added

---
+++ Day 04/TypeCasting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 67;
+    char c = 'b';
+
+    // Ab mai chahtie hon k a k andr c ko rkhwao..
+
+    // a = c;
+
+    // A b a k andr kea eyaga guess kro💭
+    // a k andr ayega 98.. ku k a apny andr int chahta hai tu hum b ki ascii value ko a k andr rkhwa daingy jo k 98 hoti hai..
+    // So mery khyal sy 98 e print o ga screen pr...
+    // let see....
+    // cout << a << endl;          //output = 98...
+
+
+    // Ab aghr m c l]k andr a ko rkhwao tu kea output ayegi like this:
+
+    c = a;
+
+    // c apny andr character chahta hai lkin a tu int hai islye m oss int ko character m convert krogi jo k 67 hai and 67 = C hota hai
+    // so screen pr yehi C print o jyega like this:
+
+    cout << c << endl;       //output = C...
+
+}
+
+
