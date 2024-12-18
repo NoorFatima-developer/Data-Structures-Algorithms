2c2ba0f524941bf6a1801924f0007b599b130113
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-19 01:55:08 +0500

    code is added

---
+++ Day 04/for_loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+// Without loop b print o jyega but mehnat zada so will use for-loop:
+// int main(){
+//     cout << " Coder Army\n";
+//     cout << " Coder Army\n";
+//     cout << " Coder Army\n";
+//     cout << " Coder Army\n";
+//     cout << " Coder Army\n";
+// }
+
+// Example 01:
+
+int main(){
+    for(int i = 0; i < 5; i++){
+        cout << " Coder Army\n";
+    }
+}
