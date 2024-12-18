193afd4d35071b0f4dc5cdd04bce728dc3094e9c
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-18 21:36:12 +0500

    code is added

--- Day 04/if_else.cpp
+++ Day 04/if_else.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int package;
-    cout << "Enter your Package: ";
-    cin >> package;
 
-    if(package > 10){
-        cout << "Accepted";
-    }
-    else{
-        cout << "Rejected";
-    }
-}
+// Example 01:
+
+// int main() {
+//     int package;
+//     cout << "Enter your Package: ";
+//     cin >> package;
+
+//     if(package > 10){
+//         cout << "Accepted";
+//     }
+//     else{
+//         cout << "Rejected";
+//     }
+// }
+
+// Example 02:
+
+// int main(){
+//     int a, b;
+//     cout << "Enter your first Number: ";
+//     cin >> a;
+
+//     cout << "Enter your second Number: ";
+//     cin >> b;
+
+//     if(a>b){
+//         cout << a << " is greater than " << b;
+//     }
+
+//     else {
+//         cout << a << " is less than " << b;
+//     }
+// }
+
