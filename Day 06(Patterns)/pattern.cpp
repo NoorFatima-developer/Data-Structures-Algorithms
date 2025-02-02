7ebadfa4e7dc71b5cec9ef356aacb7ced8dfa275
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-02 18:46:12 +0500

    code is added

--- Day 06(Patterns)/pattern.cpp
+++ Day 06(Patterns)/pattern.cpp
@@ -65,16 +65,16 @@ using namespace std;
 // 1 2 3 4 5
 // 1 2 3 4 5
 
-int main(){
-    int row, col;
+// int main(){
+//     int row, col;
 
-    for(row=1; row<=5; row++){
-    for(col=1; col<=5; col++){
-        cout << col << " ";
-    }
-    cout << endl;  // New line after each row
-}
-}
+//     for(row=1; row<=5; row++){
+//     for(col=1; col<=5; col++){
+//         cout << col << " ";
+//     }
+//     cout << endl;  // New line after each row
+// }
+// }
 
 // Q-05 Print
 
@@ -83,3 +83,14 @@ int main(){
 // 5 4 3 2 1
 // 5 4 3 2 1
 // 5 4 3 2 1
+
+int main(){
+    int row, col;
+
+    for(row=1; row<=5; row++){
+    for(col=5; col>=1; col--){
+        cout << col << " ";
+    }
+    cout << endl;  // New line after each row
+}
+}
