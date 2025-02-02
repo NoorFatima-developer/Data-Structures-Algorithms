2b9b68af7ecef3401d6f329a6593e5938c276b96
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-02 18:14:32 +0500

    code is added

--- Day 06(Patterns)/pattern.cpp
+++ Day 06(Patterns)/pattern.cpp
@@ -37,3 +37,22 @@ int main(){
         cout << endl;  // New line after each row
     }
 }
+
+// Q-03 Print
+
+// 1 1 1 1 1 
+// 2 2 2 2 2
+// 3 3 3 3 3
+// 4 4 4 4 4
+// 5 5 5 5 5
+
+int main(){
+    int row, col;
+
+    for(row=1; row<=5; row++){
+    for(col=1; col<=5; col++){
+        cout << row << " ";
+    }
+    cout << endl;  // New line after each row
+}
+}
