95601855048ecb8e2817be41214f25b42460ee49
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-06 19:11:05 +0500

    codeis added

--- Day 07(Pattern Part 02)/Pattern.cpp
+++ Day 07(Pattern Part 02)/Pattern.cpp
@@ -178,7 +178,7 @@ using namespace std;
 
 int main(){
     for(int row = 1; row <= 5; row++){
-        for(int col = 5; col>=row; col--){
+        for(int col = 5; col>=5-(row-1); col--){
             cout << col << " ";
         }
         cout << endl;  // New line after each row
