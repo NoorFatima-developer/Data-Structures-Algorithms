743c063a052874655752d494344ba78dbaeda2c4
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-06 15:45:20 +0500

    codeis added

---
+++ Day 07(Pattern Part 02)/Pattern.cpp
@@ -0,0 +1,36 @@
+// Q-11 
+
+//  *
+//  * *
+//  * * *
+//  * * * *
+//  * * * * *
+
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     for(int row = 1; row <=5; row++) {
+//         for(int col = 1; col <=row; col++) {
+//             cout << " * ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// Q-12 
+// 1 
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+int main(){
+    for(int row = 1; row <=5; row++) {
+        for(int col = 1; col <=row; col++) {
+            if(col <= row)
+                cout << col << " " ;
+        }
+        cout << endl;  // New line after each row
+    }
+}
