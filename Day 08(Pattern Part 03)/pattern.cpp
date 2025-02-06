f48eb5cf4cbf991dc7d465a441d1bb70f8fc5185
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-07 01:48:25 +0500

    codeis added

--- Day 08(Pattern Part 03)/pattern.cpp
+++ Day 08(Pattern Part 03)/pattern.cpp
@@ -1,3 +1,6 @@
+// Inn patterns mai jahan space b print krni hain or num ya * b tu i will use
+// condition mai 1 sy num-row tk inner for loop mai condition lgy gi..;
+
 #include <iostream>
 using namespace std;
 
@@ -34,21 +37,46 @@ using namespace std;
 
 // Q-02
 
+//      1
+//     2 2
+//   3 3 3
+// 4 4 4 4
+
+// int main(){
+//     // user sy input leny klye...
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+//     for(int row=1; row<=num; row++){
+//         for(int col=1; col<=num-row; col++){
+//             // print star...
+//             cout<<"  ";
+//         }
+//         for(int col=1; col<=row; col++){
+//             // print numbers...
+//             cout<< row << " ";
+//         }
+//         cout << endl;
+
+//     }
+// }
+
+// Q-03
+
 int main(){
-    // user sy input leny klye...
+     // user sy input leny klye...
     int num;
     cout << "Enter a number: ";
     cin >> num;
     for(int row=1; row<=num; row++){
         for(int col=1; col<=num-row; col++){
-            // print star...
-            cout<<"  ";
+            cout<< "  " ;
         }
-        for(int col=1; col<=row; col++){
-            // print numbers...
-            cout<< row << " ";
-        }
-        cout << endl;
+
+         for(int col=1; col<=row; col++){
+             cout << col << " ";
+         }
+         cout << endl;
 
     }
 }
