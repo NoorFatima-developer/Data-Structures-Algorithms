7b84c01fe7d8f0feffdb9e34455c1b8c19835eca
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-06 23:51:25 +0500

    codeis added

--- Day 08(Pattern Part 03)/pattern.cpp
+++ Day 08(Pattern Part 03)/pattern.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 
 int main(){
-    for(int row=1; row<=5; row++){
+
+    // user sy input leny klye...
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    for(int row=1; row<=num; row++){
         // print space...
-        for(int col=1; col<=5-row; col++){
-            cout << " " ;
-        
+        for(int col=1; col<=num-row; col++){
+            cout<<"  ";
+        }
         for(int col=1; col<=row; col++){
             // print star...
-            cout << " * " ;
+            cout<<"* " ;
         }
         cout << endl;
+        
     }
-}
 }
 
 
