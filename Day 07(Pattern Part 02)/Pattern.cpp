5e4eb7308852cfa43409a85ce742fabc21e1aaa8
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-06 19:21:45 +0500

    codeis added

--- Day 07(Pattern Part 02)/Pattern.cpp
+++ Day 07(Pattern Part 02)/Pattern.cpp
@@ -1,7 +1,6 @@
 // Is trhan k tree patterns mai row tk length rkhni hai col ki...
 
 // Q-11 
-
 //  *
 //  * *
 //  * * *
@@ -11,14 +10,14 @@
 #include <iostream>
 using namespace std;
 
-// int main() {
-//     for(int row = 1; row <=5; row++) {
-//         for(int col = 1; col <=row; col++) {
-//             cout << " * ";
-//         }
-//         cout << endl;
-//     }
-// }
+int main() {
+    for(int row = 1; row <=5; row++) {
+        for(int col = 1; col <=row; col++) {
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
 
 // Q-12 
 // 1 
@@ -27,15 +26,15 @@ using namespace std;
 // 1 2 3 4
 // 1 2 3 4 5
 
-// int main(){
-//     for(int row = 1; row <=5; row++) {
-//         for(int col = 1; col <=row; col++) {
-//             if(col <= row)
-//                 cout << col << " " ;
-//         }
-//         cout << endl;  // New line after each row
-//     }
-// }
+int main(){
+    for(int row = 1; row <=5; row++) {
+        for(int col = 1; col <=row; col++) {
+            if(col <= row)
+                cout << col << " " ;
+        }
+        cout << endl;  // New line after each row
+    }
+}
 
 // Q-13
 // 1
@@ -44,14 +43,14 @@ using namespace std;
 // 4 4 4 4
 // 5 5 5 5 5
 
-// int main(){
-//     for(int row = 1; row <= 5; row++){
-//         for(int col = 1; col <= row; col++ ){
-//             cout << row << " ";
-//         }
-//         cout << endl;  // New line after each row
-//     }
-// }
+int main(){
+    for(int row = 1; row <= 5; row++){
+        for(int col = 1; col <= row; col++ ){
+            cout << row << " ";
+        }
+        cout << endl;  // New line after each row
+    }
+}
 
 // Q-14
 // 1 
@@ -60,14 +59,14 @@ using namespace std;
 // 4 3 2 1
 // 5 4 3 2 1
 
-// int main(){
-//     for(int row = 1; row <= 5; row++){
-//         for(int col = row; col >=1; col--){
-//             cout << col << " ";
-//         }
-//         cout << endl;  // New line after each row
-//     }
-// }
+int main(){
+    for(int row = 1; row <= 5; row++){
+        for(int col = row; col >=1; col--){
+            cout << col << " ";
+        }
+        cout << endl;  // New line after each row
+    }
+}
 
 // Q-15
 // a 
@@ -76,16 +75,16 @@ using namespace std;
 // d d d d
 // e e e e e
 
-// int main(){
-//     for(int row = 1; row <= 5; row++){
-//         // row tk hi condition lgygi..
-//         char name = 'a' + (row - 1);
-//         for(int col = 1; col <= row; col++){
-//             cout << name << " ";
-//         }
-//         cout << endl;  // New line after each rowx        
-//     }
-// }
+int main(){
+    for(int row = 1; row <= 5; row++){
+        // row tk hi condition lgygi..
+        char name = 'a' + (row - 1);
+        for(int col = 1; col <= row; col++){
+            cout << name << " ";
+        }
+        cout << endl;  // New line after each rowx        
+    }
+}
 
 // Q-16(Way 01..)
 // * * * * * 
@@ -94,14 +93,14 @@ using namespace std;
 // * *
 // *
 
-// int main(){
-//     for(int row = 1; row <= 5; row++){
-//         for(int col = 5; col >=row; col--){
-//             cout << "*" << " ";
-//         }
-//     cout << endl;  // New line after each row
-//     }
-// }
+int main(){
+    for(int row = 1; row <= 5; row++){
+        for(int col = 5; col >=row; col--){
+            cout << "*" << " ";
+        }
+    cout << endl;  // New line after each row
+    }
+}
 
 
 // Q-16(Way 02..)
@@ -111,14 +110,14 @@ using namespace std;
 // * *
 // *
 
-// int main(){
-//     for(int row = 5; row >= 1; row--){
-//         for(int col = 1; col <=row; col++){
-//             cout << "*" << " ";
-//         }
-//     cout << endl;  // New line after each row
-//     }
-// }
+int main(){
+    for(int row = 5; row >= 1; row--){
+        for(int col = 1; col <=row; col++){
+            cout << "*" << " ";
+        }
+    cout << endl;  // New line after each row
+    }
+}
 
 // Q-16(Way 03..)
 // * * * * * 
@@ -127,14 +126,14 @@ using namespace std;
 // * *
 // *
 
-// int main(){
-//     for(int row = 1; row <= 5; row++){
-//         for(int col = 1; col <= 5-(row - 1); col++){
-//             cout << "*" << " ";
-//         }
-//     cout << endl;  // New line after each row
-//     }
-// }
+int main(){
+    for(int row = 1; row <= 5; row++){
+        for(int col = 1; col <= 5-(row - 1); col++){
+            cout << "*" << " ";
+        }
+    cout << endl;  // New line after each row
+    }
+}
 
 //Q-17:(Way 01..)
 // 1 2 3 4 5 
@@ -143,14 +142,14 @@ using namespace std;
 // 1 2
 // 1
 
-// int main(){
-//     for(int row = 1; row <= 5; row++){
-//         for(int col=1; col <= 5-(row - 1); col++){
-//             cout << col << " ";
-//         }
-//         cout << endl;  // New line after each row
-//     }
-// }
+int main(){
+    for(int row = 1; row <= 5; row++){
+        for(int col=1; col <= 5-(row - 1); col++){
+            cout << col << " ";
+        }
+        cout << endl;  // New line after each row
+    }
+}
 
 //Q-17:(Way 02...)
 // 1 2 3 4 5 
@@ -159,14 +158,14 @@ using namespace std;
 // 1 2
 // 1
 
-// int main(){
-//     for(int row = 5; row >= 1; row--){
-//         for(int col=1; col <= row; col++){
-//             cout << col << " ";
-//         }
-//         cout << endl;  // New line after each row
-//     }
-// }
+int main(){
+    for(int row = 5; row >= 1; row--){
+        for(int col=1; col <= row; col++){
+            cout << col << " ";
+        }
+        cout << endl;  // New line after each row
+    }
+}
 
 
 // Q-18
