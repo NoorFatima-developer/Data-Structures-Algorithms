4ec6a0b5a2f52a29a8d777768f89ab847d52d391
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-07 00:17:12 +0500

    codeis added

--- Day 08(Pattern Part 03)/pattern.cpp
+++ Day 08(Pattern Part 03)/pattern.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+// Q-01
+//                 *
+//               * *
+//             * * *
+//           * * * *
+//         * * * * *
+//       * * * * * *
+//     * * * * * * *
+//   * * * * * * * *
+// * * * * * * * * *
 
+// int main(){
+
+//     // user sy input leny klye...
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+//     for(int row=1; row<=num; row++){
+//         // print space...
+//         for(int col=1; col<=num-row; col++){
+//             cout<<"  ";
+//         }
+//         for(int col=1; col<=row; col++){
+//             // print star...
+//             cout<<"* " ;
+//         }
+//         cout << endl;
+        
+//     }
+// }
+
+// Q-02
+
+int main(){
     // user sy input leny klye...
     int num;
     cout << "Enter a number: ";
     cin >> num;
-    for(int row=1; row<=num; row++){
-        // print space...
-        for(int col=1; col<=num-row; col++){
-            cout<<"  ";
-        }
-        for(int col=1; col<=row; col++){
-            // print star...
-            cout<<"* " ;
-        }
-        cout << endl;
-        
-    }
+    for(int row=1; row<=num; row++)
 }
-
-
