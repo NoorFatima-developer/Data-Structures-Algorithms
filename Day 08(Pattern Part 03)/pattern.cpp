42c92ac22797cb3f862a49c36694428dbf5b58a8
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-06 21:48:55 +0500

    codeis added

--- Day 08(Pattern Part 03)/pattern.cpp
+++ Day 08(Pattern Part 03)/pattern.cpp
@@ -3,14 +3,17 @@ using namespace std;
 
 int main(){
     for(int row=1; row<=5; row++){
+        // print space...
         for(int col=1; col<=5-row; col++){
             cout << " " ;
         }
         for(int col=1; col<=row; col++){
+            // print star...
             cout << " * " ;
         }
         cout << endl;
     }
+
 }
 
 
