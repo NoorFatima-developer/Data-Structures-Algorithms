cdc6c4db91e13aed87e605f416605817fd374f78
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-06 18:55:46 +0500

    codeis added

--- Day 07(Pattern Part 02)/Pattern.cpp
+++ Day 07(Pattern Part 02)/Pattern.cpp
@@ -136,15 +136,32 @@ using namespace std;
 //     }
 // }
 
-//Q-17:
+//Q-17:(Way 01..)
 // 1 2 3 4 5 
 // 1 2 3 4
 // 1 2 3
 // 1 2
 // 1
+
+// int main(){
+//     for(int row = 1; row <= 5; row++){
+//         for(int col=1; col <= 5-(row - 1); col++){
+//             cout << col << " ";
+//         }
+//         cout << endl;  // New line after each row
+//     }
+// }
+
+//Q-17:(Way 02...)
+// 1 2 3 4 5 
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
 int main(){
-    for(int row = 1; row <= 5; row++){
-        for(int col=1; col <= 5-(row - 1); col++){
+    for(int row = 5; row >= 1; row--){
+        for(int col=1; col <= row; col++){
             cout << col << " ";
         }
         cout << endl;  // New line after each row
