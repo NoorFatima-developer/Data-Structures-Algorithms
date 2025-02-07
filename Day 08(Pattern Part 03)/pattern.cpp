bf4381886184caaf8b83326ebba689499c0189bf
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-07 17:22:07 +0500

    codeis added

--- Day 08(Pattern Part 03)/pattern.cpp
+++ Day 08(Pattern Part 03)/pattern.cpp
@@ -11,8 +11,6 @@
 // and jahan pr char type o ga wahan use this as well:
 // char name = 'a' + (row - 1);
 
-
-
 #include <iostream>
 using namespace std;
 
@@ -26,52 +24,46 @@ using namespace std;
 //     * * * * * * *
 //   * * * * * * * *
 // * * * * * * * * *
-
-// int main(){
-
-//     // user sy input leny klye...
-//     int num;
-//     cout << "Enter a number: ";
-//     cin >> num;
-//     for(int row=1; row<=num; row++){
-//         // print space...
-//         for(int col=1; col<=num-row; col++){
-//             cout<<"  ";
-//         }
-//         for(int col=1; col<=row; col++){
-//             // print star...
-//             cout<<"* " ;
-//         }
-//         cout << endl;
-        
-//     }
-// }
-
+int main(){
+    // user sy input leny klye...
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    for(int row=1; row<=num; row++){
+        // print space...
+        for(int col=1; col<=num-row; col++){
+            cout<<"  ";
+        }
+        for(int col=1; col<=row; col++){
+            // print star...
+            cout<<"* " ;
+        }
+        cout << endl;    
+    }
+}
 // Q-02
-
 //       1
 //     2 2
 //   3 3 3
 // 4 4 4 4
+int main(){
+    // user sy input leny klye...
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    for(int row=1; row<=num; row++){
+        for(int col=1; col<=num-row; col++){
+            // print star...
+            cout<<"  ";
+        }
+        for(int col=1; col<=row; col++){
+            // print numbers...
+            cout<< row << " ";
+        }
+        cout << endl;
 
-// int main(){
-//     // user sy input leny klye...
-//     int num;
-//     cout << "Enter a number: ";
-//     cin >> num;
-//     for(int row=1; row<=num; row++){
-//         for(int col=1; col<=num-row; col++){
-//             // print star...
-//             cout<<"  ";
-//         }
-//         for(int col=1; col<=row; col++){
-//             // print numbers...
-//             cout<< row << " ";
-//         }
-//         cout << endl;
-
-//     }
-// }
+    }
+}
 
 // Q-03
 
@@ -81,23 +73,23 @@ using namespace std;
 //   1 2 3 4
 // 1 2 3 4 5
 
-// int main(){
-//      // user sy input leny klye...
-//     int num;
-//     cout << "Enter a number: ";
-//     cin >> num;
-//     for(int row=1; row<=num; row++){
-//         for(int col=1; col<=num-row; col++){
-//             cout<< "  " ;
-//         }
+int main(){
+     // user sy input leny klye...
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    for(int row=1; row<=num; row++){
+        for(int col=1; col<=num-row; col++){
+            cout<< "  " ;
+        }
 
-//          for(int col=1; col<=row; col++){
-//              cout << col << " ";
-//          }
-//          cout << endl;
+         for(int col=1; col<=row; col++){
+             cout << col << " ";
+         }
+         cout << endl;
 
-//     }
-// }
+    }
+}
 
 // // Q-No 04:-(Way 01...)
 //         a
@@ -106,24 +98,24 @@ using namespace std;
 //   a b c d
 // a b c d e
 
-// int main(){
-//     // user sy input leny klye...
-//     int num;
-//     cout << "Enter a number: ";
-//     cin >> num;
+int main(){
+    // user sy input leny klye...
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
     
-//     for(int row=1; row<=num; row++){
-//         for(int col=1; col<=num-row; col++){
-//             cout << "  ";
-//         }
-//         char name = 'a' + (row - 1);
-//         for(char col='a'; col<=name; col++){
-//             cout << col << " ";
-//         }
-//         cout << endl;
-//     }
-//     return 0;
-// }
+    for(int row=1; row<=num; row++){
+        for(int col=1; col<=num-row; col++){
+            cout << "  ";
+        }
+        char name = 'a' + (row - 1);
+        for(char col='a'; col<=name; col++){
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
 
 // // Q-No 04:-(Way 02...)
 //         a
@@ -132,24 +124,24 @@ using namespace std;
 //   a b c d
 // a b c d e
 
-// int main(){
-//     // user sy input leny klye...
-//     int num;
-//     cout << "Enter a number: ";
-//     cin >> num;
+int main(){
+    // user sy input leny klye...
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
     
-//     for(int row=1; row<=num; row++){
-//         for(int col=1; col<=num-row; col++){
-//             cout << "  ";
-//         }
-//         for(char name = 'a'; name <='a' + (row - 1); name++){
-//             cout << name << " ";
-//         }
-//         cout << endl;
-//     }
-//     return 0;
+    for(int row=1; row<=num; row++){
+        for(int col=1; col<=num-row; col++){
+            cout << "  ";
+        }
+        for(char name = 'a'; name <='a' + (row - 1); name++){
+            cout << name << " ";
+        }
+        cout << endl;
+    }
+    return 0;
  
-// }
+}
 
 // Q-No 04:-(Way 03...)
 //         a
@@ -158,29 +150,33 @@ using namespace std;
 //   a b c d
 // a b c d e
 
-// int main(){
-//     // user sy input leny klye...
-//     int num;
-//     cout << "Enter a number: ";
-//     cin >> num;
+int main(){
+    // user sy input leny klye...
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
     
-//     for(int row=1; row<=num; row++){
-//         for(int col=1; col<=num-row; col++){
-//             cout << "  ";
-//         }
-//         for(int col=1; col<=row; col++){
-//             char name = 'a' + (col -1);
-//             cout << name << " ";
+    for(int row=1; row<=num; row++){
+        for(int col=1; col<=num-row; col++){
+            cout << "  ";
+        }
+        for(int col=1; col<=row; col++){
+            char name = 'a' + (col -1);
+            cout << name << " ";
         
-//         }
-//         cout << endl;
+        }
+        cout << endl;
         
-//     }
-//     return 0;
+    }
+    return 0;
  
-// }
+}
 
-// Q-05:
+// // Q-05:
+//       1
+//     2 1
+//   3 2 1
+// 4 3 2 1
 
 int main(){
     int num;
@@ -188,13 +184,12 @@ int main(){
     cin >> num;
     for(int row=1; row<=num; row++){
         for(int col=1; col<=num-row; col++){
-            cout << " ";
+            cout << "  ";
         }
-        for(int col=1; col<=row; col++){
-            cout << col;
+        for(int col=row; col>=1; col--){
+            cout << col << " ";
         }
         cout << endl;
 
-
-    }
+   }
 }
