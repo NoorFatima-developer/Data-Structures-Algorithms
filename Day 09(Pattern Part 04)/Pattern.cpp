0898cb9c9e1db6280842486a82c335e160327f83
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-11 21:16:39 +0500

    code is added

--- Day 09(Pattern Part 04)/Pattern.cpp
+++ Day 09(Pattern Part 04)/Pattern.cpp
@@ -78,12 +78,12 @@ int main(){
     cout << "Enter the Input: ";
     cin >> n;
 
-    for(int row = 1; row >= n; --row){
+    for(int row = n; row >= 1; row--){
         for(int col = 1; col <= n - row; ++col){
-            cout << " ";
+            cout << "  ";
         }
-        for(int col = 5; col >= 2 * row - 1; ++col){
-            cout << "*";
+        for(int col = 1; col <= 2 * row - 1; ++col){
+            cout << " *";
         }
         cout << endl;
     }
