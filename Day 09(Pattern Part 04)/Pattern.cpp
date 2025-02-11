0e0ce6f4d399fbfab537eef47585cfe71793ab13
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-11 20:06:48 +0500

    code is added

--- Day 09(Pattern Part 04)/Pattern.cpp
+++ Day 09(Pattern Part 04)/Pattern.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // Q-NO-01:
+// Formula: 1 sy 2*row-1 tk..
 
 //      *
 //     ***
@@ -31,6 +32,33 @@ using namespace std;
 
 // Q-NO-02:
 
+// int main(){
+//     int row;
+//     int col;
+//     int n;
+    
+//     cout << "Enter the Input: ";
+//     cin >> n;
+
+//     for(int row=1; row<=n; row++){
+//         // For space:(1 sy n-row tk)
+//         for(int col=1; col<=n-row; col++){
+//             cout << "  ";
+//         }
+//         // For num:(1 sy row tk)
+//         for(int col=1; col<=row; col++){
+//             cout << col <<" ";
+//         }
+//         // For decrement(row-1 sy 1 tk..) where row ends...
+//         for(int col=row-1; col>=1; col--){
+//             cout << col <<" ";
+//     }
+//         cout << endl;
+//     }
+// }
+
+// Q-NO-03:
+
 int main(){
     int row;
     int col;
@@ -39,19 +67,13 @@ int main(){
     cout << "Enter the Input: ";
     cin >> n;
 
-    for(int row=1; row<=n; row++){
-        // For space:(1 sy n-row tk)
-        for(int col=1; col<=n-row; col++){
-            cout << "  ";
+    for(int row = 1; row <= n; ++row){
+        for(int col = 1; col <= n - row; ++col){
+            cout << " ";
         }
-        // For num:(1 sy row tk)
-        for(int col=1; col<=row; col++){
-            cout << col <<" ";
+        for(int col = 5; col >= 2 * row - 1; ++col){
+            cout << "*";
         }
-        // For decrement(row-1 sy 1 tk..) where row ends...
-        for(int col=row-1; col>=1; col--){
-            cout << col <<" ";
-    }
         cout << endl;
     }
 }
