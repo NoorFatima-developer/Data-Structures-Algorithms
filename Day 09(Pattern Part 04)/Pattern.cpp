b3302d61e8a1f5a006236dcec32c26ccc768db1c
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-11 19:40:56 +0500

    code is added

--- Day 09(Pattern Part 04)/Pattern.cpp
+++ Day 09(Pattern Part 04)/Pattern.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
 
+// Q-NO-01:
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+
+// int main(){
+//     int row;
+//     int col;
+//     int n;
+    
+//     cout << "Enter the Input: ";
+//     cin >> n;
+
+//     for(int row = 1; row <= n; ++row){
+//         for(int col = 1; col <= n - row; ++col){
+//             cout << " ";
+//         }
+//         for(int col = 1; col <= 2 * row - 1; ++col){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// Q-NO-02:
+
 int main(){
     int row;
     int col;
@@ -8,14 +38,4 @@ int main(){
     
     cout << "Enter the Input: ";
     cin >> n;
-
-    for(int row = 1; row <= n; ++row){
-        for(int col = 1; col <= n - row; ++col){
-            cout << " ";
-        }
-        for(int col = 1; col <= 2 * row - 1; ++col){
-            cout << "*";
-        }
-        cout << endl;
-    }
 }
