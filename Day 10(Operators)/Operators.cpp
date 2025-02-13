92963e03334e92699042293b9a1b44e7c46829a1
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-14 04:52:23 +0500

    code is added

--- Day 10(Operators)/Operators.cpp
+++ Day 10(Operators)/Operators.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+
 // 01----------Arithmetic operations:(unary operators includes pre and post increment and decrement...(++, --) and binary operators(*, /, %) and(+, -))
 
-// i-Unary operator:
-int main(){
+    // i-Unary operator:
     // // jiski presence bri hogi wo hi output mai ayega..
     // cout << 15/2 << endl;       //output = 7;(int h tu int hi..)
     // cout << 15.0/2 << endl;  //output = 7.5;(float h tu float)
@@ -17,6 +19,7 @@ int main(){
     // int b = 10;
     // cout << --b << endl; // output = 9
     // cout << b << endl;   // output = 9
+
 
 // 02----Comparison Operators:(==, >, <, >=, <=, !=)
 // if a == b, means a == 6; answer is no.. means 0....
@@ -123,4 +126,3 @@ int main(){
 
 
 }
-
