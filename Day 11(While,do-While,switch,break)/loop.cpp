fda9332483e9d8f71299ce4f6499b1aecfedea27
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-15 21:16:55 +0500

    code is added

--- Day 11(While,do-While,switch,break)/loop.cpp
+++ Day 11(While,do-While,switch,break)/loop.cpp
@@ -1,16 +1,48 @@
-// Print Table of 8 by using while-loop...
 
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n = 8;
+
+// Print Table of 8 by using while-loop...
+// int main() {
+//     int n = 8;
+//     int i = 1;
+
+//     while(i <= 10) {
+//        cout << n*i <<  endl;
+//        i++;
+//     }
+
+//     return 0;
+// }
+
+// Print even numbers using while-loop...
+
+// int main(){
+//     int n;
+//     cout << "Enter the Number: ";
+//     cin >> n;
+
+//     int i = 2;
+//     while(i <= n){
+//         cout << i << " ";
+//         i += 2;
+//     }
+// }
+
+
+// Print odd numbers using while-loop...
+
+int main(){
+    int n;
+    cout << "Enter the Number: ";
+    cin >> n;
+
     int i = 1;
-
-    while(i <= 10) {
-       cout << "n*1" << "";
-       i++;
+    while(i <= n){
+        if(i/n){
+        cout << i << " ";
+        i += 1;
     }
-
-    return 0;
+}
 }
