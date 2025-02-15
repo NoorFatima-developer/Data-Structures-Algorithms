e016bbd827078b54a5b3f2c886215c189e0ae254
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-16 00:42:18 +0500

    code is added

--- Day 11(While,do-While,switch,break)/do_while.cpp
+++ Day 11(While,do-While,switch,break)/do_while.cpp
@@ -4,13 +4,13 @@ using namespace std;
 // Print Sum of n numbers...
 
 int main(){
-    int n, i, sum=0;
+    int n; 
     cout << "Enter the Number: ";
     cin >> n;
-
-
+    int i = 1, sum=0;
     do{
         sum = sum + i;
         i++;
+        cout << "Sum: " << sum << endl;
     }while(i < n);
 }
