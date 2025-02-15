8211e3ede8a44a470d49459e622885571dbc8f32
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-16 01:25:51 +0500

    code is added

--- Day 11(While,do-While,switch,break)/break.cpp
+++ Day 11(While,do-While,switch,break)/break.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
+// Learn Break..
+// And break ka mtlb hai k jahan pr break laga dea hai oss sy next statment ko
+// execute ni krna...
 
 int main(){
     int num;
     cout << "Enter a Number: ";
     cin >> num;
 
-    for(int i = 0; i < num; i++){
+    for(int i = 1; i <= num; i++){
         if(i == 4){
             break;
-            cout << i << " " << endl;
         }
+        cout << i << " ";
     }
+    return 0;
 }
