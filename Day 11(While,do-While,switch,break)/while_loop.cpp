8f840d70c719e4abc2dd3c0ad3f77567b4b4d5be
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-16 00:31:47 +0500

    code is added

--- Day 11(While,do-While,switch,break)/while_loop.cpp
+++ Day 11(While,do-While,switch,break)/while_loop.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 using namespace std;
 
+// Print even numbers using while-loop...(Way 01)
+// Print even numbers using while-loop...(Way 02)
+// Print odd numbers using while-loop...
+// Print Table of any number by using while-loop...
+
 
 // Print even numbers using while-loop...(Way 01)
 
@@ -54,16 +59,16 @@ using namespace std;
 
 
 // Print Table of any number by using while-loop...
-int main() {
-    int n;
-    cout << "Enter the Number: ";
-    cin >> n;
+// int main() {
+//     int n;
+//     cout << "Enter the Number: ";
+//     cin >> n;
 
-    int i = 1;
-    while(i <= n) {
-       cout << n << " * " << i << " = " << n*i <<  endl;
-       i++;
-    }
+//     int i = 1;
+//     while(i <= n) {
+//        cout << n << " * " << i << " = " << n*i <<  endl;
+//        i++;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
