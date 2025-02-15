e8d74510be1a59048df7820a232d1d1795c7f246
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-16 03:24:28 +0500

    code is added

--- Day 12(Binary to Decimal)/binary.cpp
+++ Day 12(Binary to Decimal)/binary.cpp
@@ -10,16 +10,22 @@ int main(){
     int ans = 0;
 
     while(num > 0){
+
         // remainder update:
         remainder = num % 2;
+        // remainder ko hum bitwise sy b krskty hain oss sy b 1 hi ayega..
+        // remainder = num&1;
+
         // quotient update:
         num/=2;
         // isko essy b likh skty hain..
         // num =  num / 2;
+
         // ans update:
         // ans = remainder * i + ans;
         // isko b essy likh skti hon..
         ans+= remainder * i;
+        
         // multipy update:
         i*= 10;
         // essy b likh skti hon..
