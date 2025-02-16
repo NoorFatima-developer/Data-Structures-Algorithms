e029a60896eb26b15ee328508ccde0597c5f9114
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-17 04:13:43 +0500

    code is added

---
+++ Day 13(LeetCode)/reverse_int.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+        int main() {
+            int x;
+            int ans=0, rem;
+            while (x != 0) {
+                rem = x % 10;
+                x/= 10;
+                // overflow +ve and -ve nums klye:
+                if(ans > INT_MAX/10 || ans < INT_MIN/10)
+                return 0;
+                ans = ans*10+rem;
+            }
+            return ans;
+};
