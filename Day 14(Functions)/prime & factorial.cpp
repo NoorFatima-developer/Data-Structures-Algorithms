5522cb36042fe00ed954a6c2ebdd8f2e33ea7886
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-20 19:26:45 +0500

    code is added

--- Day 14(Functions)/prime & factorial.cpp
+++ Day 14(Functions)/prime & factorial.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+// Prime no hai ya ni oska code:
+// e.g:- aghr meny 6 ko check krna hai k wo prime h ya ni tu m 2 sy 5 tk check krogi....
+bool Prime(int n){
+    if(n <= 1)
+        return false;
+    for(int i = 2; i < n; i++){
+        if(n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+// Factorial of a number:
+int factorial(int n){
+    int ans = 1;
+    for(int i = 1; i <= n; i++){
+        ans *= i;
+    }
+    return ans;
+}
+
 int main(){
     int a, b;
     cout << "Enter numbers: ";
