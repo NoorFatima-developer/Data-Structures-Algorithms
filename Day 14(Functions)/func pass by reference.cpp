6dae97f88b7fcc091fa9acd6824bd71a80cbcebb
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-20 20:50:16 +0500

    code is added

--- Day 14(Functions)/func pass by reference.cpp
+++ Day 14(Functions)/func pass by reference.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 // Pass by reference:
-void swap(int &a, int &b){
+void swap(int &a, int &b){      //same name but different parameters
     int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap(float &a, float &b){  //same name but different parameters
+                                // ab aghr m nichy oper waly swap ko e call krogi tu wo error dega ku k ye float h
+                                // or oper int type islye jb esa error ayega tu isko hum func overloaading bolty hain...
+    float temp;
     temp = a;
     a = b;
     b = temp;
@@ -16,7 +25,11 @@ int main(){
 
     swap(num1, num2);
     cout << "After swapping: " << num1 << " " << num2 << endl;
+    
+    float num3=4.8, num4=3.6;
+    swap(num3, num4);
+    cout << "After swapping: " << num3 << " " << num4 << endl;
+    
     return 0;
- 
 }
 
