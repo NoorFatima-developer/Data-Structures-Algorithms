e09194091588a97cfbd01a858556c321112b01ef
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-20 19:22:04 +0500

    code is added

--- Day 14(Functions)/prime & factorial.cpp
+++ Day 14(Functions)/prime & factorial.cpp
@@ -6,5 +6,11 @@ int main(){
     cout << "Enter numbers: ";
     cin >> a >> b;
 
+    // A is prime or not:
+    // A ka factorial:
+    // B is prime or not:
+    // B ka factorial:
+    // B-A is prime or not:
+    // B-A ka factorial:
 
 }
