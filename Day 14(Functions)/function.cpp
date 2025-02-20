f1b132695e4bb7c773a1201dd07cf6c4f199b930
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-20 19:18:33 +0500

    code is added

--- Day 14(Functions)/function.cpp
+++ Day 14(Functions)/function.cpp
@@ -8,6 +8,13 @@ int sum(int a, int b){              //Function declare...
     return ans;
 }
 
+// Ab meri req  hai k mai 2 numbers ko multiply krna chahti o but using void...
+
+void multiply(int a, int b){
+    int ans = a * b;
+    cout << "Product of " << a << " and " << b << " is: " << ans << endl;
+}
+
 // Function return nothing, so used void as a return type..but it does not return any value...
 void funct(){
     cout << "Hello from a function!" << endl;
@@ -25,5 +32,6 @@ int main(){
     int result = sum(num1, num2);
     cout << "Sum of " << num1 << " and " << num2 << " is: " << result << endl;
     funct();
+    multiply(num1, num2);
     return 0;
 }
