6fe8299daa47b4ac2c2395ccd30d163e9a626268
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-21 04:03:03 +0500

    code is added

---
+++ Day 15(C++ Problems)/conversion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// convert lower case alphabets to uppercase alphabets...
+
+char convert(char n){
+    char ans = n - 'a' + 'A';
+    return ans;
+}
+
+int main(){
+    char name;
+    cout << "Enter a conversion alphabet: ";
+    cin >> name;
+    char ans = convert(name);
+    cout << "Converted character: " << ans << endl;
+
+}
