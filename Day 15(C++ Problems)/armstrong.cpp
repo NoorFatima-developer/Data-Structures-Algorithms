1781bc3f0cb3562d9fb11df014845db29754d497
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-21 06:17:28 +0500

    code is added

--- Day 15(C++ Problems)/armstrong.cpp
+++ Day 15(C++ Problems)/armstrong.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 using namespace std;
 
+int countdigit(int num) {
+    int count = 0;
+    while (num!=0) {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+bool Armstrong(int num, int digit){
+    int n = num, rem, ans = 0;
+    while(n!=0) {
+        rem = n % 10;
+        n /= 10;
+        ans += pow(rem, digit);
+    }
+    if(ans == num)
+    return true;
+    else
+    return false;
+}
+
 int main() {
     int num;
     cout << "Enter a number: ";
     cin >> num;
-    
+    // Count digit...
+    int digit = countdigit(num);
+    // ArmStrong number...
+    cout << Armstrong(num, digit) << endl;
+
 }
