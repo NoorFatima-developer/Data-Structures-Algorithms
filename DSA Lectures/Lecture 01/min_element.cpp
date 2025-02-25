7742c3ec133fb61400f7e6c7dfc42928a5d26cdb
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-25 20:50:00 +0500

    code is added

--- DSA Lectures/Lecture 01/min_element.cpp
+++ DSA Lectures/Lecture 01/min_element.cpp
@@ -1,5 +1,16 @@
+#include <iostream>
+using namespace std;
+
 // Find minimum element in array..
 
 int main(){
     int arr[5] = {0,1,2,3,4};
+    int ans = INT_MAX;
+    
+    for(int i=0; i<5; i++){
+        if(arr[i] < ans){
+            ans = arr[i];
+        }
+    cout << "Minimum element is: " << ans << endl;
+    }
 }
