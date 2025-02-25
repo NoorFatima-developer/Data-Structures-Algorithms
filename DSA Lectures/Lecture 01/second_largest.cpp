bd296ea014512cedb83baca773842c364de89104
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-26 04:59:03 +0500

    code is add4d

---
+++ DSA Lectures/Lecture 01/second_largest.cpp
@@ -0,0 +1,13 @@
+// 5: Find the second largest element in an array of unique elements of size n. Where n>3.
+
+// aghr mai pehly largest find krleti o and then second largets krti o it would be easy to solve for me..
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Size of the array:
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+}
