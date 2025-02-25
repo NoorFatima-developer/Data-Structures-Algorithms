f944937a80e57dad90be517757988f1af637fbfe
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-25 20:48:14 +0500

    code is added

--- DSA Lectures/Lecture 01/array.cpp
+++ DSA Lectures/Lecture 01/array.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 int main(){
-    int arr[5] = {1, 2, 3, 4, 5};       //nt stores 4 bytes
+    int arr[5] = {1, 2, 3, 4, 5};       //int stores 4 bytes
+
+    // array 5 stores total 20bytes..
+
+    cout << sizeof(arr) << endl;
 
     // size of array find krny klye...
     cout << sizeof(arr)/sizeof(arr[0]) <<" ";   //20/4 -->//output = 5;
-
+    cout << endl;
     // ab int 4 bytes store krta hai ye kesy pta chlyga..
     int b;
     cout << sizeof(b) <<" ";              //output = 4;
---
+++ DSA Lectures/Lecture 01/min_element.cpp
@@ -0,0 +1,5 @@
+// Find minimum element in array..
+
+int main(){
+    int arr[5] = {0,1,2,3,4};
+}
