d5c3b432fbb0c5e76fd50d5905696ea3ba7d50e2
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-25 22:56:20 +0500

    code is add4d

--- DSA Lectures/Lecture 01/average.cpp
+++ DSA Lectures/Lecture 01/average.cpp
@@ -4,5 +4,19 @@ using namespace std;
 // Calculate the average of elements in an array of size 18.
 
 int main(){
+    int arr [5];
+    int sum = 0;
+    int n = 5;
+    cout << "Enter 5 numbers:\n";
+    for(int i = 0; i < 5; i++){
+        cin >> arr[i];
+    }
 
+    cout << "Sum of entered numbers is:\n";
+    for(int i = 0; i < 5; i++){
+        sum += arr[i];
+    }
+
+    // Displaying the average:
+    cout << "Average: " << (float)sum / n << endl;
 }
