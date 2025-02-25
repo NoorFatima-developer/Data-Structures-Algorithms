2b01c16883b78612b9f23912702f49a6fb437fc9
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-25 20:38:31 +0500

    code is added

--- DSA Lectures/Lecture 01/array.cpp
+++ DSA Lectures/Lecture 01/array.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    int arr[5] = {1, 2, 3, 4, 5};       //nt stores 4 bytes
+
+    // size of array find krny klye..
+    cout << sizeof(arr)/sizeof(arr[0]) <<" ";
+}
