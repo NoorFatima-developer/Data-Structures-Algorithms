c857b049ec28de73951c3293783a322fad3c7410
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-25 22:36:09 +0500

    code is add4d

--- DSA Lectures/Lecture 01/sum.cpp
+++ DSA Lectures/Lecture 01/sum.cpp
@@ -1,1 +1,20 @@
-// 1: Take 20 elements from user input and find its sum with the help of an array.
+// 1: Take 5 elements from user input and find its sum with the help of an array.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr [5];
+    int sum = 0;
+    cout << "Enter 5 numbers:\n";
+    for(int i = 0; i < 5; i++){
+        cin >> arr[i];
+    }
+
+    cout << "\nSum of entered numbers is:\n";
+    for(int i = 0; i < 5; i++){
+        sum += arr[i];
+        cout << sum << " ";
+    }
+
+}
