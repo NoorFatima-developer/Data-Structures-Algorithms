d723193b74bdb57b23aeb445019c8d3a78569372
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-25 20:40:42 +0500

    code is added

--- DSA Lectures/Lecture 01/array.cpp
+++ DSA Lectures/Lecture 01/array.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main(){
     int arr[5] = {1, 2, 3, 4, 5};       //nt stores 4 bytes
 
-    // size of array find krny klye..
-    cout << sizeof(arr)/sizeof(arr[0]) <<" ";
+    // size of array find krny klye...
+    cout << sizeof(arr)/sizeof(arr[0]) <<" ";   //20/4 -->//output = 5;
+
+    // ab int 4 bytes store krta hai ye kesy pta chlyga..
+    int b;
+    cout << sizeof(b) <<" ";              //output = 4;
 }
