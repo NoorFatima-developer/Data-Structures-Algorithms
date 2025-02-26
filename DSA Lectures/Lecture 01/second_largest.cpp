b47fc837324e4a45d68f475732a437f2ebcd90da
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-02-26 05:07:04 +0500

    code is add4d

--- DSA Lectures/Lecture 01/second_largest.cpp
+++ DSA Lectures/Lecture 01/second_largest.cpp
@@ -10,4 +10,41 @@ int main(){
     int n;
     cout<<"Enter the size of the array: ";
     cin>>n;
+
+    if(n<3){
+        cout<<"Size should be greater than 3!";
+        return 1;
+    }
+
+    // Get array from user:
+    int* arr = new int[n];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    // largest and second largest elements:
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for(int i = 0; i<n; i++){
+        if(arr[i]>largest){
+            secondLargest = largest; //yahan abhi INT_MIN ayega
+            largest = arr[i];        // yahan pehla no ayega array ka..
+        } 
+        // ye condition iss case klye lagae hai k aghr oper wali cond largest k lye false hoe lkin secondlargest klye true hoe
+        // tu oss case m hmain second largest ko update krna hoga tu phr wo oss case m else wali cond mai second largets ko update krdeyga...
+
+        else if(arr[i] > secondLargest){
+            secondLargest = arr[i]; 
+        }
+
+        // print secondlargest..
+        cout<<"Second largest element: "<<secondLargest<<endl;
+    }
+    
+    // delete dynamically allocated memory:
+    delete[] arr;
+    return 0;
+ 
 }
