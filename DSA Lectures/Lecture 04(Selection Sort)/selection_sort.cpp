060749e69addbde6f9be1d586b173f0cf9c029c0
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-03-05 17:24:22 +0500

    code is added

--- DSA Lectures/Lecture 04(Selection Sort)/selection_sort.cpp
+++ DSA Lectures/Lecture 04(Selection Sort)/selection_sort.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
 using namespace std;
 
+// Sort numbers in ascending order..
+
+// int main(){
+//     int n;
+//     cout << "Enter the size of the array: ";
+//     cin >> n;
+
+//     int* arr = new int[n];
+//     cout << "Enter " << n << " elements: ";
+//     for (int i = 0; i < n; i++) { 
+//         cin >> arr[i];  
+//     }
+
+//     for(int i = 0; i<n; i++){
+//         int index = i;
+
+//         for(int j = i + 1; j < n; j++){
+//             if(arr[index] > arr[j]){
+//                 index = j;
+//             }
+//         }
+//         swap(arr[i], arr[index]);
+//     }
+
+
+//     cout << "Sorted array: ";
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+// Sort numbers in descending order..
+
+// int main(){
+//     int n;
+//     cout << "Enter the size of the array: ";
+//     cin >> n;
+
+//     int* arr = new int[n];
+//     cout << "Enter " << n << " elements: ";
+//     for (int i = 0; i < n; i++) { 
+//         cin >> arr[i];  
+//     }
+
+//     for(int i = 0; i<n; i++){
+//         int index = i;
+
+//         for(int j = i + 1; j < n; j++){
+//             if(arr[index] < arr[j]){
+//                 index = j;
+//             }
+//         }
+//         swap(arr[i], arr[index]);
+//     }
+
+
+//     cout << "Sorted array: ";
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+
+
+// Second way of sorting ...largest num ko end pr lek jana hai...
+
 int main(){
     int n;
     cout << "Enter the size of the array: ";
@@ -12,21 +82,23 @@ int main(){
         cin >> arr[i];  
     }
 
-    for(int i = 0; i<n; i++){
+    for(int i = n-1; i>=0; i--){
         int index = i;
 
-        for(int j = i + 1; j < n; j++){
-            if(arr[index] > arr[j]){
+        for(int j = 0; j < i; j++){
+            if(arr[index] < arr[j]){
                 index = j;
             }
         }
         swap(arr[i], arr[index]);
     }
-    
+
+
     cout << "Sorted array: ";
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
 
+    delete[] arr; 
     return 0;
 }
