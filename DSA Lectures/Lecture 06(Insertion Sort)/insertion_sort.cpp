b7678a7b789b21d2f4487842b75837f6d86160b0
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-03-05 21:21:34 +0500

    code is added

---
+++ DSA Lectures/Lecture 06(Insertion Sort)/insertion_sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int *arr = new int[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i = 1; i < n - 1; i++){
+        for(int j = i; j > 0; j--){
+            if(arr[j] < arr[j - 1]){
+                swap(arr[j], arr[j - 1]);
+            }
+            else
+            break;
+        }
+    }
+
+    cout << "Sorted array: ";
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    
+        delete[] arr;  // Free dynamically allocated memory
+        return 0;
+
+}
