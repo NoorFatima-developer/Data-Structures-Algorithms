a4753417732ca05c72f4f008ccd4b35441035d10
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-03-05 21:14:00 +0500

    code is added

--- DSA Lectures/Lecture 04(Selection Sort)/selection_sort.cpp
+++ DSA Lectures/Lecture 04(Selection Sort)/selection_sort.cpp
@@ -3,69 +3,69 @@ using namespace std;
 
 // Sort numbers in ascending order..
 
-// int main(){
-//     int n;
-//     cout << "Enter the size of the array: ";
-//     cin >> n;
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
 
-//     int* arr = new int[n];
-//     cout << "Enter " << n << " elements: ";
-//     for (int i = 0; i < n; i++) { 
-//         cin >> arr[i];  
-//     }
+    int* arr = new int[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) { 
+        cin >> arr[i];  
+    }
 
-//     for(int i = 0; i<n; i++){
-//         int index = i;
+    for(int i = 0; i<n; i++){
+        int index = i;
 
-//         for(int j = i + 1; j < n; j++){
-//             if(arr[index] > arr[j]){
-//                 index = j;
-//             }
-//         }
-//         swap(arr[i], arr[index]);
-//     }
+        for(int j = i + 1; j < n; j++){
+            if(arr[index] > arr[j]){
+                index = j;
+            }
+        }
+        swap(arr[i], arr[index]);
+    }
 
 
-//     cout << "Sorted array: ";
-//     for (int i = 0; i < n; i++) {
-//         cout << arr[i] << " ";
-//     }
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 // Sort numbers in descending order..
 
-// int main(){
-//     int n;
-//     cout << "Enter the size of the array: ";
-//     cin >> n;
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
 
-//     int* arr = new int[n];
-//     cout << "Enter " << n << " elements: ";
-//     for (int i = 0; i < n; i++) { 
-//         cin >> arr[i];  
-//     }
+    int* arr = new int[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) { 
+        cin >> arr[i];  
+    }
 
-//     for(int i = 0; i<n; i++){
-//         int index = i;
+    for(int i = 0; i<n; i++){
+        int index = i;
 
-//         for(int j = i + 1; j < n; j++){
-//             if(arr[index] < arr[j]){
-//                 index = j;
-//             }
-//         }
-//         swap(arr[i], arr[index]);
-//     }
+        for(int j = i + 1; j < n; j++){
+            if(arr[index] < arr[j]){
+                index = j;
+            }
+        }
+        swap(arr[i], arr[index]);
+    }
 
 
-//     cout << "Sorted array: ";
-//     for (int i = 0; i < n; i++) {
-//         cout << arr[i] << " ";
-//     }
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
