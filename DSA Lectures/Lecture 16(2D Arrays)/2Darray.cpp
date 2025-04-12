dd4f356c9fa3134868ce65c66da414cda3651571
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-04-12 09:47:39 +0500

    2d array executed

--- DSA Lectures/Lecture 16(2D Arrays)/2Darray.cpp
+++ DSA Lectures/Lecture 16(2D Arrays)/2Darray.cpp
@@ -13,7 +13,9 @@ void printcol(int arr[][4], int row, int col)
 int main() {
     
     //create 2d array
-    int arr[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int arr1[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int arr2[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int ans[3][4];
 
     //--- print all the values in array row wise...
     // for(int row = 0; row < 3; row++)
@@ -23,16 +25,30 @@ int main() {
     //--- print all the values in array column wise..
     // printcol(arr, 3, 4);
 
-    // Find an element in an array..
-    int x = 7;
+
+    // 02---Find an element in an array..
+    // int x = 7;
+
+    // for(int row = 0; row < 3; row++)
+    // for(int col = 0; col < 4; col++)
+    // {
+    //     if(arr[row][col] == x){
+    //         cout << "Yes";
+    //         return 0;
+    //     }
+    // }
+    // cout << "No";
+
+    // 03---Add 2 matrix:
 
     for(int row = 0; row < 3; row++)
     for(int col = 0; col < 4; col++)
     {
-        if(arr[row][col] == x){
-            cout << "Yes";
-            return 0;
-        }
+        ans[row][col] = arr1[row][col] + arr2[row][col];
     }
-    cout << "No";
+
+    for(int row = 0; row < 3; row++)
+    for(int col = 0; col < 4; col++)
+    cout << ans[row][col] << " ";
+
 }
