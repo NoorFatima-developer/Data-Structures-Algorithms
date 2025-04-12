7e61697f0ed9259d85354f1db0469099f9987c8c
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-04-12 12:23:51 +0500

    2d array executed

--- DSA Lectures/Lecture 16(2D Arrays)/2DVector.cpp
+++ DSA Lectures/Lecture 16(2D Arrays)/2DVector.cpp
@@ -8,9 +8,12 @@ int main() {
 
     // user sy input leny klye..
     int n, m;
+    cout << "Enter rows and cols: ";
     cin >> n >> m;
     // col 4 and hr 1 ko initialize krdo 1 sy...
-    vector<vector<int>>matrix(n, vector<int>(m,1));
+    // lkin jb hum user sy input lety hain fer hmain initialize krny ki zrort ni hoti
+    // ku k hum log khud dery hoty hain values manually...
+    vector<vector<int>>matrix(n, vector<int>(m, 1));
 
     // cin..
     for(int i = 0; i < n; i++)
@@ -19,12 +22,14 @@ int main() {
 
     // cout..
     for(int i = 0; i < n; i++)
-    for(int j = 0; j < m; j++)
-    cout << matrix[i][j] << " ";
-    cout << endl;
+    {
+        for(int j = 0; j < m; j++)
+        cout << matrix[i][j] << " ";
+        cout << endl;
+    }
 
     // Aghr mjy rows or cols ni dien hoe tu i will use this to find..
-    cout << "Rows = " << matrix.size();
-    cout << endl;
-    cout << "Cols = " << matrix[0].size();
+    // cout << "Rows = " << matrix.size();
+    // cout << endl;
+    // cout << "Cols = " << matrix[0].size();
 }
